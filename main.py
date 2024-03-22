70229102503829c7c1e7e064ba39445320448454
alzheimeer <alzheimeer@hotmail.com>
2024-03-21 20:34:15 -0500

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from routes import api_blueprint  
 from flask_cors import CORS
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -35,4 +36,4 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
